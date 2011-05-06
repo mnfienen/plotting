5d39d8deca10b1a83936410405d82537cc9b9cc7
Mike Fienen <mnfienen@usgs.gov>
2011-05-06 09:22:17 -0500

    Added x tick labels

--- bar_plot.py
+++ bar_plot.py
@@ -14,16 +14,21 @@ width = 0.5
 inds = np.arange(len(indat['Standard_Method']))
 ax2.bar(inds,indat['Standard_Method'],width,color='red')
 ax2.bar(inds+width,indat['Double_Ring'],width,color='blue')
+plt.legend(['Standard Method','Double Ring'])
 ax.bar(inds,indat['Standard_Method'],width,color='red')
 ax.bar(inds+width,indat['Double_Ring'],width,color='blue')
 ax.xaxis.tick_bottom()
 
 ax2.set_ylim(-1,20)
-ax.set_ylim(40,45)
-
+ax.set_ylim(40,60)
 ax2.spines['top'].set_visible(False)
 ax.spines['bottom'].set_visible(False)
 
-plt.subplots_adjust(wspace=0.05)
+plt.xticks(inds)
+xtickNames = ax2.set_xticklabels(indat['land_use'])
+plt.setp(xtickNames, rotation=25, fontsize=8)
+
+ax.xaxis.set_visible(False)
+plt.savefig('barchart.eps')
 
 plt.show()
